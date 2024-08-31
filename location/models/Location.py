9109a1060492957ae1f7fdc268dd55236f13d133
Arne Coomans <app+github@arnecoomans.nl>
2024-08-31 10:39:49 +0200

    cleanup

--- location/models/Location.py
+++ location/models/Location.py
@@ -338,30 +338,15 @@ class Location(BaseModel):
         if self.location.parent.parent:
           return self.location.parent.parent
     return None
-  # def countryslug(self):
-  #   if self.location:
-  #     if self.location.parent:
-  #       if self.location.parent.parent:
-  #         return self.location.parent.parent.slug
-  #   return None
   def region(self):
     if self.location:
       if self.location.parent:
         return self.location.parent
     return None
-  # def regionslug(self):
-  #   if self.location:
-  #     if self.location.parent:
-  #       return self.location.parent.slug
-  #   return None
   def department(self):
     if self.location:
       return self.location
     return None
-  # def departmentslug(self):
-  #   if self.location:
-  #     return self.location.slug
-  #   return None
   
   ''' Activity or Location logic '''
   def isActivity(self):

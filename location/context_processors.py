c9531cf106a6012f6a7117cf10a513aaef6d2da7
Arne Coomans <app+github@arnecoomans.nl>
2024-09-15 15:23:16 +0200

    improved displaying in action list and show center location for distance calculations

--- location/context_processors.py
+++ location/context_processors.py
@@ -9,4 +9,5 @@ def setting_data(request):
     'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     'default_order': settings.DEFAULT_ORDER if hasattr(settings, 'DEFAULT_ORDER') else 'distance',
     'allow_unauthenticated_read_comments': settings.ALLOW_UNAUTHENTICATED_READ_COMMENTS if hasattr(settings, 'ALLOW_UNAUTHENTICATED_READ_COMMENTS') else False,
+    'departure_center': settings.DEPARTURE_CENTER if hasattr(settings, 'DEPARTURE_CENTER') else 'Domplein, Utrecht',
   }

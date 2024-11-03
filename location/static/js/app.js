50f77c71354ec666fee7d2aa139f04fac2a37a6e
Arne Coomans <app+github@arnecoomans.nl>
2024-11-03 21:03:22 +0100

    auto-fade out alerts after 10 seconds

--- location/static/js/app.js
+++ location/static/js/app.js
@@ -1,3 +1,27 @@
+// Function to apply fade-out to alerts
+function applyFadeOut() {
+  var duration = 10 * 1000; // X times 1000 ms = X seconds
+
+  $('.alert').each(function() {
+    var $this = $(this);
+
+    // Check if the alert already has a fade-out timer to prevent duplication
+    if (!$this.data('fade-out-set')) {
+      $this.data('fade-out-set', true); // Mark this alert as having a fade-out timer
+
+      // Set the timer for fade-out
+      setTimeout(function() {
+        $this.fadeOut(1000, function() { // 1000 ms = 1 second fade-out
+          message = $this.text();
+          message = message.replace('(Undo)', '').trim();
+          console.log('Removing alert "' + message + '"');
+          $this.remove();
+        });
+      }, duration);
+    }
+  });
+}
+
 $(document).ready(function() {
   /**  Auto-Capitalize
    * Auto-capitalize the first letter of each word in an input field
@@ -33,4 +57,14 @@ $(document).ready(function() {
     minLength: 2 // Only start suggesting after 2 characters have been typed
                 // Issue #248 experiment with autosuggest tags: show from 0 characters
   });
+
+  // Select all alert messages and set a timer to fade them out
+  $(document).ready(function() {
+    applyFadeOut();
+  });
+
+  // Reapply fade-out logic after AJAX content is loaded
+  $(document).ajaxComplete(function() {
+    applyFadeOut();
+});
 });

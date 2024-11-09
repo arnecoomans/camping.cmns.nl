8688cd27f0e935c4a9aa7423f5729bcbaf2a188b
Arne Coomans <app+github@arnecoomans.nl>
2024-11-09 15:14:49 +0100

    fix name of translations view

--- location/views/translations.py
+++ location/views/translations.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
-''' Cheat Class
+''' Translation Cheat Class
     This class holds translations for often used terms from database content.
     By storing these terms in this class, it will appear via django-admin makemessages -a
     and retain the translations when translating.

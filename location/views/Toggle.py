e175d11a4142cff98420931cb4ce655cdba2b319
Arne Coomans <app+github@arnecoomans.nl>
2024-11-17 09:03:27 +0100

    move reusable functions from toggle to jsonhelper

--- location/views/Toggle.py
+++ location/views/Toggle.py
@@ -6,14 +6,15 @@ from django.shortcuts import redirect
 from django.http import JsonResponse
 from django.utils.html import escape
 from django.utils.text import slugify
-from django.db.models import BooleanField, ManyToManyField
+#from django.db.models import BooleanField, ManyToManyField
 
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
-from location.models.Location import Location, Category, Chain
-from location.models.Profile import Profile, VisitedIn
-from location.models.Tag import Tag
-from location.models.Comment import Comment
+# from location.models.Location import Location, Category, Chain
+# from location.models.Profile import Profile, VisitedIn
+# from location.models.Tag import Tag
+# from location.models.Comment import Comment
+from .json.jsonhelper import JSONHelper
 # from location.models.Media import Media
 
 ''' Toggle View
@@ -27,174 +28,27 @@ from location.models.Comment import Comment
     - Example: /json/location:domaine-les-gandins/like/ (No value)
     - Example: /json/location:domaine-les-gandins/category/restaurant/
 '''
-class ToggleAttribute(View):
-
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
-    self.model = None
-    self.object = None
-    self.slug = None
-    self.field = None
-    self.value = None
-    self.parent = None
-
-    self.models = {
-      'tag': Tag,
-      'category': Category,
-      'chain': Chain,
-      'location': Location,
-      'profile': Profile,
-    }
-    self.status = 200
-    self.messages = []
-
-  ''' Get Value from URL
-      @scope: private
-      @param key: key to be fetched from URL
-      @return: value from URL
-      Retrieves the parameter from URL, POST or GET request
-  '''
-  def __get_value_from_url(self, key):
-    if key in self.kwargs:
-      ''' Key is in URL '''
-      return self.kwargs[key]
-    elif self.request.GET.get(key, False):
-      ''' Key is in GET request '''
-      return self.request.GET.get(key)
-    elif self.request.POST.get(key, False):
-      ''' Key is in POST request '''
-      return self.request.POST.get(key)
-    else:
-      ''' Key is not found'''
-      return False
-  
-  ''' Get Argument From Class Cache
-      @scope: private
-      @param key: key to be fetched from Class Cache
-      @return: value from Class Cache
-      Retrieves the parameter from class cache, falls back to 
-      __get_value_from_url if not found
-  '''
-  def __get_arg(self, key):
-    if getattr(self, key) == None:
-      ''' Cache is not yet filled, fetch value from URL '''
-      setattr(self, key, self.__get_value_from_url(key))
-    return getattr(self, key)
-  
-  ''' Get Model
-      @scope: private
-      @return: Model that holds the object that needs a value toggled
-  '''
-  def __get_model(self):
-    if self.model == None:
-      ''' Fetch Model Name from URL, GET or POST '''
-      model = self.__get_value_from_url('model')
-      ''' Verify if model is supported '''
-      if model in self.models:
-        ''' Model is supported, store reference to Model '''
-        self.model = self.models[model]
-      else:
-        self.status = 500
-        self.messages.append(('danger', f"[122] { _('model "{}" not supported').format(model.__name__).capitalize }"))
-        self.model = False
-    return self.model
-  
-  def __get_object(self):
-    if self.object == None:
-      ''' Fetch Model '''
-      model = self.__get_model()
-      ''' Set Object if Slug is supplied'''
-      if self.__get_slug():
-        ''' Select Object within Model By Slug'''
-        if 'slug' in [field.name for field in model._meta.get_fields()]:
-          ''' Model has Slug Field, get object by slug '''
-          try:
-            self.object = model.objects.get(slug__iexact=self.slug)
-          except model.DoesNotExist:
-            self.status = 404
-            self.messages.append(('danger', f"[282] { _('model "{}" requires slug but no object found with slug {}').format(model.__name__, self.slug).capitalize() }"))
-            self.object = False
-        elif 'user' in [field.name for field in model._meta.get_fields()]:
-          ''' Model has User Field (example: profile), get or create object where user = current user '''
-          try:
-            # @TODO: Check if user is authenticated
-            self.object = model.objects.get(user__username__iexact=self.__get_slug())
-          except model.DoesNotExist:
-            self.status = 404
-            self.messages.append(('danger', f"[286] { _('model "{}" requires user but no object found for user {}').format(model.__name__, self.__get_slug()).capitalize() }"))
-            self.object = False
-        else:
-          ''' Lookup field not available in Model, log an error message '''
-          self.status = 500
-          self.messages.append(('danger', f"[854] { _('lookup for model "{}" not supported').format(model.__name__).capitalize() }"))
-          self.object = False
-      else:
-        ''' No Slug supplied, fall back to defaults '''
-        if 'user' in [field.name for field in model._meta.get_fields()]:
-          ''' If Model has User Field (example: profile), get or create object where user = current user '''
-          try:
-            self.object = model.objects.get_or_create(user=self.request.user)[0]
-          except model.DoesNotExist:
-            self.status = 404
-            self.messages.append(('danger', f"[284] { _('model "{}" requires user but no object found for user {}').format(model.__name__, self.request.user).capitalize() }"))
-            self.object = False
-    return self.object
-  
-  def __get_slug(self):
-    return self.__get_arg('slug')
-
-  def __get_field(self):
-    if self.field == None:
-      field = self.__get_arg('field')
-      if field in [field.name for field in self.__get_model()._meta.get_fields()]:
-        self.field = field
-      else:
-        self.status = 500
-        self.messages.append(('danger', f"[127] { _('field "{}" not supported for model "{}"').format(field, self.__get_model().__name__).capitalize() }"))
-        self.field = False
-    return self.field
-  
-  def __get_field_display(self):
-    return self.__get_field().replace('_', ' ').capitalize()
-
-  def __get_value(self):
-    if self.value == None:
-      if self.__get_arg('value'):
-        ''' Detect parent but not for URL's '''
-        if ':' in self.__get_arg('value') and not '://' in self.__get_arg('value'):
-          self.parent =self.__get_arg('value').split(':')[0] 
-          self.value = '-'.join(self.__get_arg('value').split(':')[1:])
-        else:
-          self.value = self.__get_arg('value')
-      else:
-        self.value = False
-    return self.value
-  
-  def __get_value_display(self):
-    return self.__get_value().replace('-', ' ').title()
-  
-  def __get_current_value(self):
-    return getattr(self.__get_object(), self.__get_field())
+class ToggleAttribute(JSONHelper):
   
   ''' Toggle Functions '''
   def __toggleBoolean(self, field):
     try:
-      setattr(self.__get_object(), self.__get_field(), not self.__get_current_value())
-      self.__get_object().save()
-      self.messages.append(('success', f"{ _('toggled {} to {}').format(self.__get_field_display(), getattr(self.__get_object(), self.__get_field())).capitalize() } { self.__get_undo_link() }"))
+      setattr(self.get_object(), self.get_field(), not self.get_current_value())
+      self.get_object().save()
+      self.messages.append(('success', f"{ _('toggled {} to {}').format(self.__get_field_display(), getattr(self.get_object(), self.get_field())).capitalize() } { self.get_undo_link() }"))
     except Exception as e:
       self.status = 500
-      self.messages.append(('danger', f"[137] { _('error when toggling {} of {}: {}').format(self.__get_field(), self.__get_object(), escape(e)).capitalize()}"))
+      self.messages.append(('danger', f"[137] { _('error when toggling {} of {}: {}').format(self.get_field(), self.get_object(), escape(e)).capitalize()}"))
 
   def __toggleManyToMany(self, field):
     ''' Fetch textual Value - should be slug of the object '''
-    value = self.__get_value()
+    value = self.get_value()
     if not value:
       self.status = 500
       self.messages.append(('danger', f"[181] { _('value is required but missing').capitalize() }"))
       return False
     ''' Get the model of the ManyToManyField '''
-    value_model = self.__get_model()._meta.get_field(self.__get_field()).related_model
+    value_model = self.get_model()._meta.get_field(self.get_field()).related_model
     ''' Handle parent/child relations '''
     if self.parent:
       defaults = { 'name': self.parent.replace('-', ' ').title(), 'user': self.request.user }
@@ -205,14 +59,14 @@ class ToggleAttribute(View):
     else:
       parent = None
     ''' Get or Create the value object '''
-    defaults = { 'name': self.__get_value_display(), 'user': self.request.user }
+    defaults = { 'name': self.get_value_display(), 'user': self.request.user }
     if 'parent' in [field.name for field in value_model._meta.get_fields()]:
       defaults['parent'] = parent
     if 'slug' in [field.name for field in value_model._meta.get_fields()]:
       value_object = value_model.objects.get_or_create(slug=slugify(value.lower()), defaults=defaults)
     elif 'username' in [field.name for field in value_model._meta.get_fields()]:
       value_object = value_model.objects.get_or_create(username=value.lower(), defaults=defaults)
-    elif self.__get_field() == 'link':
+    elif self.get_field() == 'link':
       try:
         value_object = (value_model.objects.get(id=value), False)
       except ValueError:
@@ -222,70 +76,56 @@ class ToggleAttribute(View):
       except value_model.DoesNotExist:
         self.status = 404
         self.messages.append(('danger', f"[404] { _('value "{}" not found').format(value).capitalize() }"))
-        return self.__return_response()
+        return self.return_response()
     else:
       self.status = 500
-      self.messages.append(('danger', f"[520s] { _('lookup for model "{}" not supported in {}: {}').format(value_model.__name__, self.__get_model().__name__, value_model._meta.get_fields()).capitalize() }"))
-      return self.__return_response()
+      self.messages.append(('danger', f"[520s] { _('lookup for model "{}" not supported in {}: {}').format(value_model.__name__, self.get_model().__name__, value_model._meta.get_fields()).capitalize() }"))
+      return self.return_response()
     if value_object[1]:
       self.messages.append(('info', f"{ _('{} {} created').format(value_model.__name__, value_object[0].name).capitalize() }"))
     value_object = value_object[0]
     ''' Toggle the value '''
-    if value_object in getattr(self.__get_object(), self.__get_field()).all():
+    if value_object in getattr(self.get_object(), self.get_field()).all():
       ''' Value is already in the ManyToManyField: Remove it '''
-      getattr(self.__get_object(), self.__get_field()).remove(value_object)
+      getattr(self.get_object(), self.get_field()).remove(value_object)
       value_object_name = (
         value_object.name if hasattr(value_object, 'name') and value_object.name 
         else value_object.get_title() if hasattr(value_object, 'get_title') 
         else value_object.username
       )
-      self.messages.append(('success', f"{ _('removed "{}" from {} {}').format(value_object_name, self.__get_field(), self.__get_object().name).capitalize() } { self.__get_undo_link() }"))
+      self.messages.append(('success', f"{ _('removed "{}" from {} {}').format(value_object_name, self.get_field(), self.get_object().name).capitalize() } { self.get_undo_link() }"))
     else:
       ''' Value should be added '''
       try:
-        getattr(self.__get_object(), self.__get_field()).add(value_object)
+        getattr(self.get_object(), self.get_field()).add(value_object)
         value_object_name = (
           value_object.name if hasattr(value_object, 'name') and value_object.name 
           else value_object.get_title() if hasattr(value_object, 'get_title') 
           else value_object.username
         )
-        self.messages.append(('success', f"{ _('added "{}" to {} {}').format(value_object_name, self.__get_field(), self.__get_object()).capitalize() } { self.__get_undo_link() }"))
+        self.messages.append(('success', f"{ _('added "{}" to {} {}').format(value_object_name, self.get_field(), self.get_object()).capitalize() } { self.get_undo_link() }"))
       except Exception as e:
         self.status = 500
-        self.messages.append(('danger', f"[152] { _('Error when adding {} to {} of {}: {}').format(value, self.__get_field(), self.__get_object(), escape(e)) }"))    
+        self.messages.append(('danger', f"[152] { _('Error when adding {} to {} of {}: {}').format(value, self.get_field(), self.get_object(), escape(e)) }"))    
 
-  def __get_undo_url(self):
-    resolver = self.request.resolver_match.url_name
-    kwargs = {
-      'model': self.__get_model().__name__.lower(),
-      'field': self.__get_field(),
-    }
-    if 'slug' in resolver.lower():
-      kwargs['slug'] = self.__get_slug()
-    if self.__get_value():
-      resolver += 'WithValue' if 'WithValue' not in resolver else ''
-      kwargs['value'] = self.__get_value()
-    return reverse_lazy("location:" + resolver, kwargs=kwargs)
   
-  def __get_undo_link(self):
-    return f'(<a href="{ self.__get_undo_url() }" class="toggable">{ _('undo').capitalize() })</a>'
   
-  def __return_response(self):
-    field = self.__get_field()
-    if field in ['favorite', 'dislike', 'ignored_tags']:
-      field = 'action'
-    response = {
-      '__meta': {
-        'url': self.request.path,
-        'resolver': self.request.resolver_match.url_name,
-        'user': self.request.user.username if self.request.user.is_authenticated else False,
-      },
-      'status': self.status,
-      'messages': self.messages,
-      'undo-url': self.__get_undo_url(),
-      'field': str(field) + 'list',
-    }
-    return JsonResponse(response, status=self.status)
+  # def return_response(self):
+  #   field = self.get_field()
+  #   if field in ['favorite', 'dislike', 'ignored_tags']:
+  #     field = 'action'
+  #   response = {
+  #     '__meta': {
+  #       'url': self.request.path,
+  #       'resolver': self.request.resolver_match.url_name,
+  #       'user': self.request.user.username if self.request.user.is_authenticated else False,
+  #     },
+  #     'status': self.status,
+  #     'messages': self.messages,
+  #     'undo-url': self.get_undo_url(),
+  #     'field': str(field) + 'list',
+  #   }
+  #   return JsonResponse(response, status=self.status)
   
   ''' Get function 
       @scope: public
@@ -300,23 +140,23 @@ class ToggleAttribute(View):
     if not request.user.is_authenticated:
       self.status = 401
       self.messages.append(('danger', f"[401] { _('user is not authenticated').capitalize() }"))
-      return self.__return_response()
+      return self.return_response()
     ''' Get Field 
         Field Access the Object in the Model
     '''
-    field = self.__get_field()
+    field = self.get_field()
     ''' Verify Progress '''
     if field: 
-      field_type =  self.__get_model()._meta.get_field(self.__get_arg('field')).__class__.__name__
+      field_type =  self.get_model()._meta.get_field(self.get_arg('field')).__class__.__name__
       ''' Based on Field Type, toggle the value '''
       if field_type == 'BooleanField':
         self.__toggleBoolean(field)
       elif field_type == 'ManyToManyField':
         self.__toggleManyToMany(field)
       else:
-        self.messages.append(('danger', f"[192] { _('Field "{}" with type "{}" not supported for model "{}"').format(field, field_type, self.__get_model().__name__) }"))
+        self.messages.append(('danger', f"[192] { _('Field "{}" with type "{}" not supported for model "{}"').format(field, field_type, self.get_model().__name__) }"))
     ''' Return response '''
-    return self.__return_response()
+    return self.return_response()
 
 
 ''' Toggle Exceptions '''

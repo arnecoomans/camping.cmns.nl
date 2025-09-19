c2b189ae715e48124725aac72334ac9fac8bcde5
Arne Coomans <app+github@arnecoomans.nl>
2025-09-19 13:20:42 +0200

    serve robots.txt

--- vknt/urls.py
+++ vknt/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+  path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    )),
   path('', include('location.urls')),
   path('json/', include('cmnsd.urls')),
   path('admin/', admin.site.urls),

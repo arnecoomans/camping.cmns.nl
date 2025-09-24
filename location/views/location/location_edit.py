6ba86285d0246050a3397dede748e03c6a11cab9
Arne Coomans <app+github@arnecoomans.nl>
2025-09-24 20:28:51 +0200

    fixed bug when adding location with link

--- location/views/location/location_edit.py
+++ location/views/location/location_edit.py
@@ -197,7 +197,7 @@ class AddLocation(CreateView):
         'user': self.request.user,
         'name': self.request.POST.get('link-title', None)
       })
-      location.link.add(link[0])
+      location.links.add(link[0])
       messages.add_message(self.request, messages.INFO, f"{ _('added link') }: \"{ link[0].get_title() }\" { _('to') } { location.name }.")
     except IntegrityError as e:
       suggested_location = Location.objects.get(name__iexact=form.cleaned_data['name'])
@@ -420,7 +420,7 @@ class addLinkToLocation(UpdateView):
           visibility=request.POST.get('visibility', 'c'),
           primary=True if request.POST.get('primary', False) else False,
         )
-        location.link.add(link)
+        location.links.add(link)
         messages.add_message(self.request, messages.INFO, f"{ _('created and') } { _('added link') }: \"{ link.get_title() }\" to { location.name }.")
     else:
       messages.add_message(self.request, messages.ERROR, f"{ _('you did not supply a valid URL') }.")

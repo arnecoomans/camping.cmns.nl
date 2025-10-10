de7a0eb53b90a839b7814515da0869808a091ea5
Arne Coomans <app+github@arnecoomans.nl>
2025-10-10 11:38:27 +0200

    inherit base model Tag from cmnsd

--- location/models/Tag.py
+++ location/models/Tag.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext_lazy as __
+from django.utils.text import slugify
 
 from django.urls import reverse_lazy
 
 from cmnsd.models.cmnsd_basemodel import BaseModel, VisibilityModel
+from cmnsd.models.Tag import Tag
 # from .base_model import BaseModel
 
-class Tag(VisibilityModel,BaseModel):
-  slug                = models.CharField(max_length=64, unique=True, help_text=f"{ _('Identifier in URL') } ({ _('automatically generated') })")
-  name                = models.CharField(max_length=128, help_text=_('Name of tag'))
-  parent              = models.ForeignKey("self", on_delete=models.CASCADE, related_name='children', null=True, blank=True)
+class Tag(VisibilityModel,Tag):
+  # slug                = models.CharField(max_length=64, unique=True, help_text=f"{ _('Identifier in URL') } ({ _('automatically generated') })")
+  # name                = models.CharField(max_length=128, help_text=_('Name of tag'))
+  # parent              = models.ForeignKey("self", on_delete=models.CASCADE, related_name='children', null=True, blank=True)
 
   list_as_choices     = (
     ('n', __('Tag is advantage or disadvantage', 'neither')),
@@ -23,21 +25,29 @@ class Tag(VisibilityModel,BaseModel):
 
   class Meta:
     ordering = ['-list_as', 'name']
+    # constraints = [
+    #   models.UniqueConstraint(fields=['parent', 'name'], name='unique_name_per_parent')
+    # ]
 
-  def __str__(self) -> str:
-    return self.display_name()
+  # def __str__(self) -> str:
+  #   return self.display_name()
+  
   def get_absolute_url(self):
     return reverse_lazy('location:tag', args=[self.slug])
   
-  def display_name(self) -> str:
-    name = ''
-    if self.parent:
-      name += self.parent.name + ': '
-    name += self.name
-    return name
+  # def save(self, *args, **kwargs):
+  #   if not self.name and self.slug:
+  #     self.name = self.slug.replace('-', ' ').replace('_', ' ').replace('+', ' ').title()
+  #   if not self.slug and self.name:
+  #     self.slug = slugify(self.name)
+  #   super().save(*args, **kwargs)
 
-  class Meta:
-    ordering = ['name']
+  # def display_name(self) -> str:
+  #   name = ''
+  #   if self.parent:
+  #     name += self.parent.name + ': '
+  #   name += self.name
+  #   return name
 
   def getLocations(self):
     result = []

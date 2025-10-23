e0b4da46e2cf49a99b9d894bc396fab3380c0e84
Arne Coomans <app+github@arnecoomans.nl>
2025-10-23 17:21:03 +0200

    more cleanup on urls

--- location/urls.py
+++ location/urls.py
@@ -21,17 +21,17 @@ urlpatterns = [
   path('location/add/', views.AddLocation.as_view(), name='AddLocation'),
   
   path('location/<str:slug>/', views.LocationView.as_view(), name='location'),
-  path('location/<str:slug>/edit/', views.EditLocation.as_view(), name='EditLocation'),
-  path('location/<str:slug>/list/', views.AddLocationToList.as_view(), name='LocationAddList'),
+  # path('location/<str:slug>/edit/', views.EditLocation.as_view(), name='EditLocation'),
+  # path('location/<str:slug>/list/', views.AddLocationToList.as_view(), name='LocationAddList'),
 
   path('location/<str:slug>/reset/', views.ResetLocationData.as_view(), name='ResetLocation'),
   path('location/<str:slug>/distance_to_home/', views.GetDistanceToHome.as_view(), name='DistanceToHome'),
   path('location/<str:slug>/description/add/', views.AddDescriptionToLocation.as_view(), name='AddDescriptionToLocation'),
-  path('location/description/add:<int:pk>/', views.AddLocationToDescription.as_view(), name='AddLocationToDescription'),
-  path('location/description/edit:<int:pk>/', views.editDescription.as_view(), name='EditDescription'),
-  path('location/<str:slug>/links/add/', views.addLinkToLocation.as_view(), name='AddLinkToLocation'),
-  path('location/<str:slug>/links/delete:<linkid>/', views.deleteLinkFromLocation.as_view(), name='DeleteLinkFromLocation'),
-  path('location/<location_slug>/links/edit:<pk>/', views.editLinkAtLocation.as_view(), name='EditLinkAtLocation'),
+  # path('location/description/add:<int:pk>/', views.AddLocationToDescription.as_view(), name='AddLocationToDescription'),
+  # path('location/description/edit:<int:pk>/', views.editDescription.as_view(), name='EditDescription'),
+  # path('location/<str:slug>/links/add/', views.addLinkToLocation.as_view(), name='AddLinkToLocation'),
+  # path('location/<str:slug>/links/delete:<linkid>/', views.deleteLinkFromLocation.as_view(), name='DeleteLinkFromLocation'),
+  # path('location/<location_slug>/links/edit:<pk>/', views.editLinkAtLocation.as_view(), name='EditLinkAtLocation'),
 
   path('activities/', views.ActivityListView.as_view(), name='activities'),
   path('activities/<country>/', views.ActivityListView.as_view(), name='ListActivitiesByCountry'),
@@ -40,7 +40,7 @@ urlpatterns = [
 
   path('activity/add/', views.AddLocation.as_view(), name='AddActivity'),
   path('activity/<str:slug>/', views.LocationView.as_view(), name='activity'),
-  path('activity/<str:slug>/edit/', views.EditActivity.as_view(), name='EditActivity'),
+  # path('activity/<str:slug>/edit/', views.EditActivity.as_view(), name='EditActivity'),
 
   path('search/', views.AllSearchView.as_view(), name='search'),
   path('search/<country>/', views.AllSearchView.as_view(), name='SearchByCountry'),
@@ -53,9 +53,9 @@ urlpatterns = [
   path('comments/by:<username>/', views.CommentByUserListView.as_view(), name='CommentsByUser'),
   
   path('tags/', views.TagListView.as_view(), name='tags'),
-  path('tags/add/to:<str:slug>/', views.AddTagToLocation.as_view(), name='AddTagToLocation'),
+  # path('tags/add/to:<str:slug>/', views.AddTagToLocation.as_view(), name='AddTagToLocation'),
   path('tag/add/', views.AddTag.as_view(), name='AddTag'),
-  path('tag/add/to:<str:slug>/', views.AddTag.as_view(), name='AddTagTo'),
+  # path('tag/add/to:<str:slug>/', views.AddTag.as_view(), name='AddTagTo'),
   path('tag/<str:slug>/', views.TagView.as_view(), name='tag'),
   path('tag/<str:slug>/edit/', views.EditTag.as_view(), name='EditTag'),
   
@@ -77,14 +77,14 @@ urlpatterns = [
   path('list/<str:slug>/<id>:<location>/down/', views.ListLocationDown.as_view(), name="ListLocationDown"),
 
   path('profile/', views.ProfileView.as_view(), name='profile'),
-   path('profile/visit/add/', views.AddVisit.as_view(), name='AddVisit'),
+  path('profile/visit/add/', views.AddVisit.as_view(), name='AddVisit'),
   path('profile/visit/edit:<pk>/', views.EditVisit.as_view(), name='EditVisit'),
 
   path('session/allow_google_maps/', views.ToggleGoogleMapsSession.as_view(), name='MapsPermissionSession'), #Profile
   path('profile/allow_google_maps//', views.ToggleGoogleMapsProfile.as_view(), name='MapsPermissionProfile'), #Profile
   
-  path('chain/add/', views.AddChain.as_view(), name='AddChainTo'),
-  path('chain/add/<str:slug>/', views.AddChain.as_view(), name='AddChainTo'),
+  # path('chain/add/', views.AddChain.as_view(), name='AddChainTo'),
+  # path('chain/add/<str:slug>/', views.AddChain.as_view(), name='AddChainTo'),
   
   path('stack/add/<str:slug>/', views.AddMediaToLocation.as_view(), name='AddMediaToLocation'),
   path('stack/list/<str:slug>/', views.StackView.as_view(), name='MediaStack'),
@@ -92,4 +92,4 @@ urlpatterns = [
   path('image/<str:filename>', views.MediaStreamView.as_view(), name='MediaStream'),
   path('register/', views.SignUpView.as_view(), name='register'),
 
-  
+]
